91343ca0cc34c7c47fb07385cc0da1c14b850059
Emmanuel Epie <eepien@gmail.com>
2025-05-31 10:15:10 -0700

    Updated the oop_ex2.py code.

--- oop_ex2.py
+++ oop_ex2.py
@@ -14,13 +14,14 @@ class Human:
         print(f'{self.name} is eating...')
 
 class Student(Human):
-    def __init__(self, name, degree_pgm, dept, age, id_num):
+    def __init__(self, name, degree_pgm, dept, age, id_num, instructor):
         super().__init__(name, age, id_num)
         self.name = name
         self.degree_pgm = degree_pgm
         self.dept = dept
+        self.instructor = instructor
     def listen(self):
-        print(f'Student {self.name} is listening to lectures...')
+        print(f'Student {self.name} is listening to lectures from {self.instructor.name}...')
     def question(self):
         print(f'Student {self.name} is asking a question...')
 
@@ -30,13 +31,12 @@ class Instructor(Human):
     def grade(self):
         print(f'Instructor {self.name} is grading assignments...')
 
+instr1 = Instructor('Einstein', 55, '576AZ')
 
-stud1=Student('Epie', 'BSc', "CSC", 32, 'CSC123')
+
+stud1=Student('Epie', 'BSc', "CSC", 32, 'CSC123', instr1)
 stud1.breath()
 stud1.listen()
 stud1.question()
 
-inst1 = Instructor('Einstein', 55, '576AZ')
-inst1.eat()
-inst1.teach()
-inst1.grade()
+
